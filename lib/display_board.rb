ef71cb6c57e5e92a8a061e8831d4023808a28317
Eduardo Ferreira <eddy2555@hotmail.com>
2020-08-07 22:21:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+
+
+
+
+
+
+
+ puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
